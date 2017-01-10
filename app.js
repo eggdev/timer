55507f4b13a26889b7c8ac6dba1aad27d8971196
Dev Eggers <eggers.brendan@gmail.com>
2017-01-10 12:27:28 -0500

    Completes the timer and the subtracting and cumulative splits

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 
 Vue.component('sub-split',{
   props: ['split'],
-  template: '<li>{{ split.split }}</li>'
+  template: '<li class="split-time">{{ split.split }}</li>'
 })
 
 
 Vue.component('cum-split',{
   props: ['split'],
-  template: '<li>{{ split.split }}</li>'
+  template: '<li class="split-time">{{ split.split }}</li>'
 })
 
 var vm = new Vue({
@@ -15,7 +15,7 @@ var vm = new Vue({
   data: {
     minutes: 00,
     seconds: 00,
-    hundreths:00,
+    hundreths: 00,
     splitMin: 00,
     splitSec: 00,
     splitHund: 00,
@@ -23,19 +23,20 @@ var vm = new Vue({
     subtractedSplit: '',
     cumSplitsArray: [],
     subSplitsArray: [],
-    buttontext: 'STOP',
+    clockisRunning: false,
+    buttontext: 'CLEAR',
   },
   methods: {
     startTimer: function(){
       let self = this;
-      console.log('hello I started');
+      self.buttontext = 'STOP';
+      self.clockisRunning = true;
       self.running = setInterval(function(){
         self.time++;
-
-        self.hundreths++
+        self.hundreths+=01
         if(self.hundreths === 99){
           self.hundreths = 00;
-          self.seconds +=1;
+          self.seconds +=01;
           if(self.seconds === 59){
             self.seconds = 00;
             self.minutes +=1;
@@ -45,33 +46,34 @@ var vm = new Vue({
     },
     getSplit: function(){
       let self = this;
+      if(self.clockisRunning){
+        let mins = self.minutes
+        let secs = (self.seconds).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
+        let hunds = (self.hundreths).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
+        let splitM = self.splitMin
+        let splitS = (self.splitSec).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
+        let splitH = (self.splitHund).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
 
-      let mins = self.minutes
-      let secs = (self.seconds).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
-      let hunds = (self.hundreths).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
-      let splitM = self.splitMin
-      let splitS = (self.splitSec).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
-      let splitH = (self.splitHund).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
+        self.cumulativeSplit = mins+":"+secs+"."+hunds;
+        self.subtractedSplit = splitM+":"+splitS+"."+splitH;
 
-      self.cumulativeSplit = mins+":"+secs+"."+hunds;
-      self.subtractedSplit = splitM+":"+splitS+"."+splitH;
+        self.cumSplitsArray.push({split: self.cumulativeSplit});
 
-      self.cumSplitsArray.push({split: self.cumulativeSplit});
+        if(self.subSplitsArray.length === 0){
+          self.subSplitsArray.push({split: self.cumulativeSplit});
+        }else {
+          self.subSplitsArray.push({split: self.subtractedSplit});
+        }
 
-      if(self.subSplitsArray.length === 0){
-        self.subSplitsArray.push({split: self.cumulativeSplit});
-      }else {
-        self.subSplitsArray.push({split: self.subtractedSplit});
+        // console.log(self.cumulativeSplit);
+        // console.log(self.subtractedSplit);
+
+        clearInterval(self.subtracting);
+        self.splitHund = 00;
+        self.splitSec = 00;
+        self.splitMin = 00;
+        self.subtractSplit();  
       }
-
-      console.log(self.cumulativeSplit);
-      console.log(self.subtractedSplit);
-
-      clearInterval(self.subtracting);
-      self.splitHund = 00;
-      self.splitSec = 00;
-      self.splitMin = 00;
-      self.subtractSplit();
     },
     subtractSplit: function(){
       let self = this;
@@ -89,11 +91,29 @@ var vm = new Vue({
     },
     stopTimer: function(){
       let self = this;
-      self.getSplit();
-      // console.log(self.cumulativeSplit);
-      // console.log(self.subtractedSplit);
-      clearInterval(self.running);
-      clearInterval(self.subtracting);
+      self.buttontext = 'CLEAR';
+      if(self.clockisRunning === false){
+        console.log(self.subSplitsArray);
+        while(self.subSplitsArray.length > 0){
+          self.subSplitsArray.pop();
+          self.cumSplitsArray.pop();
+        }
+        self.clearClock();
+      }else {
+        self.clockisRunning = false;
+        self.getSplit();
+        clearInterval(self.running);
+        clearInterval(self.subtracting);
+      }
+    },
+    clearClock: function(){
+      let self = this;
+      self.minutes = 00;
+      self.seconds = 00;
+      self.hundreths = 00;
+      self.splitMin = 00;
+      self.splitSec = 00;
+      self.splitHund = 00;
     }
   }
 });
